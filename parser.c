1fcd1bac29543f55e8d3335069f89bae99a5d88c
oumaima ouyassine <83994637+oumaimaoys@users.noreply.github.com>
2023-11-06 11:12:29 +0100

    implemented parsing functions

--- parser.c
+++ parser.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
-#include "tree.h"
+#include <ctype.h>
+#include <stdlib.h>
+#include "parser.h" 
+
+// helper functions
+char* slice_string(const char* source, int startIndex) {
+    return strdup(source + startIndex);
+}
+
+char* slice_prefix(const char* source, int endIndex){
+    size_t sliceLength = endIndex;
+    char* slicedString = (char*)malloc(sliceLength + 1);
+    if (slicedString != NULL) {
+        strncpy(slicedString, source, sliceLength);
+        slicedString[sliceLength] = '\0';
+    }
+}
 
 // we gonna assume for now that the input's syntax is correct 
 
 tree parse_program(char* source_code){
     tree parse_tree = initiate_tree(); // we create an emptry tree node
-    char* root = parse_identifier(source_code)->info; // we parse the root 
+    char* root = parse_expression(source_code)->info; // we parse the root 
     strcpy(parse_tree->info, root); // we put the identifier in the root, which is the starting token
     
     // we get the slice after = to get the expression because ebnf : token = experession
@@ -13,37 +29,135 @@ tree parse_program(char* source_code){
     for(int i = 0; source_code[i] != "=" ; i++){
         startIndex++;
     }
-
-    int sliceLength = strlen(source_code) - startIndex;
-    char* slice = (char*)malloc(sliceLength + 1);
-    strncpy(slice, source_code + 0, sliceLength);
-    slice[sliceLength] = '\0';
+    char* slice = slice_string(source_code, startIndex);
 
     // we parse the expression
     parse_expression(slice);
 
     return parse_tree;
-
 }
 
-void parse_expression(char* source_code){
+tree parse_expression(char* source_code){
     // there are three types of expressions
+    int len = strlen(source_code);
 
-    // expression starts with (
-
-    // expression stars with letter/number
-
-    // expression starts with {
-
-    // expression starts with [
+    switch (*source_code) {
+        case '(':
+            parse_parentheses(source_code + 1);
+            break;
+        case '{':
+            parse_recurssives(source_code + 1);
+            break;
+        case '[':
+            parse_optionals(source_code + 1);
+            break;
+        case '"':
+            parse_literal(source_code + 1);
+            break;
+        case '|':
+            parse_alternatives(source_code + 1);
+            break;
+        case ' ':
+            break;
+        default:
+            if (isalpha(*source_code)) {
+                parse_identifier(source_code);
+            }
+    }
+    
 }
 
-void parse_alternatives(char* source_code){}
+int parse_parentheses(char* source_code){
+    int par_flag = 0; // initially false, turns true for open parentheses adn false when parentheses closes
+    int char_len = strlen(source_code);
+    int endIndex = strlen;
 
-void parse_optionals(char* source_code){}
+    for(int i = 0; source_code[i]< char_len ;i++){
+        if(source_code[i] == '('){
+            par_flag = 1;
+        }
+        else if(par_flag == 1 && source_code[i] == ')'){
+            par_flag = 0;
+        }
+        else if(par_flag == 0 && source_code == ')'){
+            int endIndex = i - 1;
+            break;
+        }
+    }
+    return endIndex;
+}
 
-void parse_recurssives(char* source_code){}
+void parse_alternatives(char* source_code){
+    
+}
 
-tree parse_identifier(char* source_code){
+int parse_optionals(char* source_code){
+    int brackets_flag = 0; // initially false, turns true for open parentheses adn false when parentheses closes
+    int char_len = strlen(source_code);
+    int endIndex = strlen;
 
+    for(int i = 0; source_code[i]< char_len ;i++){
+        if(source_code[i] == '[]'){
+            brackets_flag = 1;
+        }
+        else if(brackets_flag == 1 && source_code[i] == ']'){
+            brackets_flag = 0;
+        }
+        else if(brackets_flag == 0 && source_code == ']'){
+            int endIndex = i - 1;
+            break;
+        }
+    }
+    return endIndex;
+}
+
+int parse_recurssives(char* source_code){
+    int flag = 0; // initially false, turns true for open parentheses adn false when parentheses closes
+    int char_len = strlen(source_code);
+    int endIndex = strlen;
+
+    for(int i = 0; source_code[i]< char_len ;i++){
+        if(source_code[i] == '{'){
+            flag = 1;
+        }
+        else if(flag == 1 && source_code[i] == '}'){
+            flag = 0;
+        }
+        else if(flag == 0 && source_code == '}'){
+            int endIndex = i - 1;
+            break;
+        }
+    }
+    return endIndex;
+}
+
+int parse_literal(char* source_code){ // anything betwenn ""
+    int char_len = strlen(source_code);
+    int endIndex = strlen;
+
+    for(int i = 0; source_code[i]< char_len ;i++){
+        if(source_code[i] == '"'){
+           endIndex = i-1;
+           break;
+        }
+    }
+    
+    return endIndex;
+}
+
+int parse_identifier(char* source_code){
+    int char_len = strlen(source_code);
+    int endIndex = 0;
+    int temp = 0;
+    for(int i = 0; source_code[i]< char_len ;i++){
+        if(isalpha(source_code[i]) && temp==i){
+            temp++;
+        }
+        else{
+            endIndex = temp;
+            break;
+        }
+        
+    }
+    return endIndex;
 }
--- parser.h
+++ parser.h
@@ -1,11 +1,17 @@
-void parse_program(char* source_code);
+#include "tree.h"
 
-void parse_expression(char* source_code);
+tree parse_program(char* source_code);
 
-void parse_alternatives(char* source_code);
+tree parse_expression(char* source_code);
 
-void parse_optionals(char* source_code);
+int parse_alternatives(char* source_code);
 
-void parse_recurssives(char* source_code);
+int parse_optionals(char* source_code);
 
-void parse_identifier(char* source_code);
+int parse_recurssives(char* source_code);
+
+int parse_literal(char* source_code);
+
+int parse_identifier(char* source_code);
+
+int parse_parentheses(char* source_code);
